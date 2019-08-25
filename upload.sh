abded9948d02ae01432c37c1c9681354e4ea959a
liyongjie <iyongjie@yeah.net>
2019-08-26 00:35:47 +0800

    sh

--- upload.sh
+++ upload.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
 echo '开始验证'
 pod spec lint MDAspects.podspec --use-libraries --allow-warnings --verbose
+say 验证完成 推送开始
 echo '开始推送'
 pod trunk push MDAspects.podspec --allow-warnings --use-libraries --verbose
+say 推送完成 推送完成
